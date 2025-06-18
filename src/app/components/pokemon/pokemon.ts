fc63129d664a68f58910fabba9f2f5901e9bee4f
lucas <lv011297@gmail.com>
2025-06-18 12:58:18 -0300

    pagina com estilização

--- src/app/components/pokemon/pokemon.ts
+++ src/app/components/pokemon/pokemon.ts
@@ -3,41 +3,61 @@ import { PokemonService } from '../../services/pokemon';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+
 @Component({
   selector: 'app-pokemon',
   imports: [CommonModule, FormsModule, PokemonComponent],
   templateUrl: 'pokemon.html',
+  styleUrls: ['pokemon.scss'],
+  standalone: true
 })
 export class PokemonComponent implements OnInit {
 
   pokemon: any;
   nome = '';
   erro: string = '';
+  tipo: string = '';
 
-  constructor(private pokemonService: PokemonService) {}
+  constructor(private pokemonService: PokemonService) { }
   ngOnInit() {
     // Inicialização se necessário
   }
 
-buscarPokemon() {
-  this.erro = '';
-  this.pokemon = null;
+  buscarPokemon() {
+    this.erro = '';
+    this.pokemon = null;
 
-  const nome = this.nome.trim().toLowerCase();
+    const nome = this.nome.trim().toLowerCase();
 
-  if (!nome) {
-    this.erro = 'Digite um nome de Pokémon.';
-    return;
-  }
-
-  this.pokemonService.getPokemon(nome).subscribe({
-    next: (res) => {
-      this.pokemon = res;
-    },
-    error: () => {
-      this.erro = `Pokémon "${this.nome}" não encontrado.`;
+    if (!nome) {
+      this.erro = 'Digite um nome de Pokémon.';
+      return;
     }
-  });
-}
+
+    this.pokemonService.getPokemon(nome).subscribe({
+      next: (res) => {
+        const tipo = res.types?.[0]?.type?.name || 'desconhecido';
+        const hp = res.stats?.find((stat: any) => stat.stat.name === 'hp')?.base_stat || 0;
+        const ataque = res.stats?.find((stat: any) => stat.stat.name === 'attack')?.base_stat || 0;
+        const defesa = res.stats?.find((stat: any) => stat.stat.name === 'defense')?.base_stat || 0;
+        const velocidade = res.stats?.find((stat: any) => stat.stat.name === 'speed')?.base_stat || 0;
+        const movimentos = res.moves?.map((move: any) => move.move.name) || [];
+
+        this.pokemon = {
+          ...res,
+          tipo,
+          hp,
+          ataque,
+          defesa,
+          velocidade,
+          habilidades: res.abilities?.map((ability: any) => ability.ability.name) || [],
+          movimentos: res.moves?.map((move: any) => move.move.name) || [],
+        };
+      },
+      error: () => {
+        this.erro = `Pokémon "${this.nome}" não encontrado.`;
+      }
+    });
+  }
 
 }
